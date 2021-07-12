a32af6d2908b5b88b15136bc6513790ec8712d84
Zaldana <rooben.zaldana@gmail.com>
2021-07-12 18:56:59 -0400

    working bounce & comp paddle, css art

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// Get the computer paddle element
+const computerPaddle = document.querySelector('.computer-paddle');
+
+// Get the computer paddle element
+const ball = document.querySelector('.ball');
+
 // Size of the game area (in px)
 const GAME_AREA_WIDTH = 700;
 const GAME_AREA_HEIGHT = 500;
@@ -9,8 +15,11 @@ const PADDLE_WIDTH = 20;
 // Size of the ball (in px)
 const BALL_SIZE = 20;
 
-// Get the computer paddle element
-const computerPaddle = document.querySelector('.computer-paddle');
+//BALL x - position, y - position, x - velocity, and y - velocity
+let ballXposition = 420;
+let ballXvelocity = 1;
+let ballYposition = 250;
+let ballYvelocity = 1;
 
 // The y-velocity of the computer paddle
 let computerPaddleYPosition = 0;
@@ -19,12 +28,67 @@ let computerPaddleYVelocity = 1;
 // Update the pong world
 function update() {
 
+    // Update the ball's x position
+    ballXposition = ballXposition + ballXvelocity;
+
+    // Apply the ball  x-position 
+    ball.style.left = `${ballXposition}px`;
+
+    // Update the bals's x position
+    ballYposition = ballYposition + ballYvelocity;
+
+    // Apply the ball  x-position 
+    ball.style.top = `${ballYposition}px`;
+
     // Update the computer paddle's position
     computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
+
+    //If paddle hits bottom
+    if (computerPaddleYPosition === 500) {
+
+        computerPaddleYVelocity = computerPaddleYVelocity * -1;
+
+    }
+
+    if (computerPaddleYPosition === 0) {
+
+        computerPaddleYVelocity = computerPaddleYVelocity * -1;
+
+    }
+    
+    //If ball hits left
+    if (ballXposition === 0) {
+
+        ballXvelocity = ballXvelocity * -1;
+        
+    }
+
+    //If ball hits right
+    if (ballXposition === 830) {
+
+        ballXvelocity = ballXvelocity * -1;
+
+
+    }
+
+    //If ball hits top
+    if (ballYposition === 0) {
+
+        ballYvelocity = ballYvelocity * -1;
+
+
+    }
+
+    //If ball hits bottom
+    if (ballYposition === 580) {
+
+        ballYvelocity = ballYvelocity * -1;
+
+    }
 }
 
 // Call the update() function every 35ms
-setInterval(update, 35);
+setInterval(update, 5);
